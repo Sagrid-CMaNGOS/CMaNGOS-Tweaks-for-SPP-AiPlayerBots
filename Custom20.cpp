b8ad5f93fc77ae51c4bf57474c4fb4895a5acc90
Sagrid-CMaNGOS <sagrid.confluxorder@gmail.com>
2025-10-21 13:50:08 +0200

    Add in MountValues.cpp - Inject Custom Mounts

--- Custom20.cpp
+++ Custom20.cpp
@@ -89,3 +89,56 @@ if (requester && ai->HasStrategy("debug mount", BotState::BOT_STATE_NON_COMBAT))
     return false; // Prevent mount action
 }
 #endif
+
+ // =============================================
+ // Add in MountValues.cpp - Inject Custom Mounts
+ // =============================================
+
+std::vector<MountValue> MountListValue::Calculate()
+{
+    std::vector<MountValue> mounts;
+
+#ifdef MANGOSBOT_ZERO
+    if (bot->GetLevel() >= 20 && bot->GetLevel() < 40)
+    {
+        std::vector<uint32> custom20;
+        switch (bot->getRace())
+        {
+            case RACE_HUMAN:      custom20 = {50001, 50002}; break;
+            case RACE_ORC:        custom20 = {50003, 50004}; break;
+            case RACE_DWARF:      custom20 = {50005, 50006}; break;
+            case RACE_NIGHTELF:   custom20 = {50007, 50008}; break;  
+            case RACE_UNDEAD:     custom20 = {50009, 50010}; break;
+            case RACE_TAUREN:     custom20 = {50011, 50012}; break;
+            case RACE_GNOME:      custom20 = {50013, 50014}; break;
+            case RACE_TROLL:      custom20 = {50015, 50016}; break;
+        }
+
+        for (uint32 spell : custom20)
+        {
+            if (!bot->HasSpell(spell))
+                bot->learnSpell(spell, false); // learn spell if necessary
+
+            mounts.push_back(MountValue(spell)); // Adding to mount list
+        }
+    }
+#endif
+
+    // ----------------------------
+    // Adding Mounts from Inventory
+    // ----------------------------
+    
+    for (auto& mount : AI_VALUE2(std::list<Item*>, "inventory items", "mount"))
+        mounts.push_back(MountValue(mount));
+
+    // ------------------------------
+    // Adding Mount from known spells
+    // ------------------------------
+    
+    for (PlayerSpellMap::iterator itr = bot->GetSpellMap().begin(); itr != bot->GetSpellMap().end(); ++itr)
+        if (itr->second.state != PLAYERSPELL_REMOVED && !itr->second.disabled && !IsPassiveSpell(itr->first))
+            if(MountValue::IsMountSpell(itr->first))
+                mounts.push_back(MountValue(itr->first));
+
+    return mounts;
+}
